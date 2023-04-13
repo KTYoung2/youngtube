873f64b0a95021d96a4bda8bff0184bbcbcc8779
kty <xodud9625@hanmail.net>
2023-04-13 22:17:49 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -1,24 +1,34 @@
 import express from "express";
+import logger from "morgan";
+/**미들웨어를 직접 만들어서 사용할 수 있지만  "morgan"이 좀 더 정교함
+ GET, path, satus code, 응답시간 이 모든 정보를 가지고 있음.
+*/
+const PORT = 4000;
 
 
 //1.express application 생성
 const app = express();
 
-const PORT = 4000;
+
 
 //2.get request 응답(처리)
-
 const handleHome = (req,res) => {
-    return res.send("<h1>사랑해<h1>");
+    return res.send("Home");
 };
 
-const handleLogin = (req,res) => {
-    return res.send("Login here~");
+const handlelogin = (req,res) => {
+    return res.send("login");
 };
 
 
+//순서 중요 ! 1.use(미들웨어) 2.get(path) -> express는 js처럼 모든걸 위에서 아래 순으로 실행시킴.
+app.use(logger("tiny"));
+/* (morgan)logger 함수는 미들웨어를 리턴해줌
+"combined" , "common" , "dev", "short", "tiny"
+*/
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/login", handlelogin);
+
 
 const handleListening = () => console.log(`Server listening on port http://localhost/:${PORT}🚀`);
 
