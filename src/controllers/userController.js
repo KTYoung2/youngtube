dd3483c67ffca2554efd90459f969042b26219c3
kty <xodud9625@hanmail.net>
2023-04-26 23:05:08 +0900

    commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,19 @@
+import User from "../models/User";
 
-export const join = (req, res) => res.send("JOIN");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "createAccount" });
+
+export const postJoin = async (req, res) => {
+    const { email, username, password, name, location } = req.body;
+    await User.create({
+        email, 
+        username, 
+        password,
+        name, 
+        location
+    });   
+    return res.redirect("/login");
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
 export const login = (req, res) => res.send("Login User");
